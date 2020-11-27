0c457312dfca23afe15204c1ddbe7b8a3cbb123e
Max <k1071303@gmail.com>
2020-11-27 16:10:55 +0800

    Finish detail page UI.

---
+++ iBadminton/ViewController/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  iBadminton
+//
+//  Created by Max Kai on 2020/11/27.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
